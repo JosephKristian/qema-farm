99143dcd94ca224d3d7a45f1cba4c0b00dd1fe1b
Joseph Kristian Tanudjaja <joseph@ipaymu.com>
2025-05-13 12:00:41 +0800

    🐛 Fix(invest): validation on deletion

--- src/pages/user/Investasi.jsx
+++ src/pages/user/Investasi.jsx
@@ -1,4 +1,4 @@
-import React, {useReducer, useEffect, useState} from 'react';
+import React, { useReducer, useEffect, useState } from 'react';
 import { AiOutlineSearch, AiOutlineClose } from 'react-icons/ai';
 import Modal from 'react-modal';
 import Footer from '../../components/Footer';
@@ -25,12 +25,14 @@ const Investasi = () => {
   const [visibleModal, setVisibleModal] = useState(false);
   const [noAccountModal, setNoAccountModal] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [deleteConfirm, setDeleteConfirm] = useState({ type: '', show: false });
+
 
   useEffect(() => {
     retrieveAllGoat();
     retrieveAllFood();
     retrieveAllMaintenance();
-    return () => {}
+    return () => { }
   }, [])
 
   const showTheModal = (title, description) => {
@@ -70,7 +72,7 @@ const Investasi = () => {
       showTheModal('Terjadi Kesalahan!', error.toString());
     }
   }
-  
+
   const retrieveAllMaintenance = async () => {
     try {
       await getAllMaintenance().then(
@@ -90,20 +92,20 @@ const Investasi = () => {
   const renderGoat = () => {
     return (
       <div className='flex flex-col items-center'>
-        {/* Search */ }
-        <div div className = 'flex flex-row w-1/3 border-2 border-[#5C5C5C] rounded-[10px] items-center self-center mb-11' >
+        {/* Search */}
+        <div className='flex flex-row w-1/3 border-2 border-[#5C5C5C] rounded-[10px] items-center self-center mb-11' >
           <input type='text' placeholder='Cari nama kambing' onChange={(e) => setGoatSearch(e.target.value)} className='flex-1 text-sm text-[#333333] font-normal px-2 py-3 outline-none bg-transparent' />
           <AiOutlineSearch size={32} color='#858585' className='mr-2 cursor-pointer' />
         </div>
 
-        {/* Sex Options */ }
-        <div div className = 'flex flex-row items-center self-center space-x-4 mb-14' >
+        {/* Sex Options */}
+        <div className='flex flex-row items-center self-center space-x-4 mb-14' >
           <button onClick={() => goatSex === 'Jantan' ? setGoatSex('') : setGoatSex('Jantan')} className={goatSex === 'Jantan' ? 'text-white text-sm font-medium bg-[#145412] border-2 rounded-full px-6 py-2' : 'text-[#145412] text-sm font-medium border-[#145412] border-2 rounded-full px-6 py-2'}>Jantan</button>
           <button onClick={() => goatSex === 'Betina' ? setGoatSex('') : setGoatSex('Betina')} className={goatSex === 'Betina' ? 'text-white text-sm font-medium bg-[#145412] border-2 rounded-full px-6 py-2' : 'text-[#145412] text-sm font-medium border-[#145412] border-2 rounded-full px-6 py-2'}>Betina</button>
         </div>
 
-        {/* Options Menu */ }
-        <div div className='w-full h-fit grid grid-cols-4 lg:gap-12 md:gap-6 gap-4'>
+        {/* Options Menu */}
+        <div className='w-full h-fit grid grid-cols-4 lg:gap-12 md:gap-6 gap-4'>
           {
             goat.filter(e => e.name.toLowerCase().includes(goatSearch.toLowerCase().trim()) && e.sex.toLowerCase().includes(goatSex.toLowerCase().trim())).map(element => (
               <div onClick={() => setConfirmGoat(element)} className='bg-white flex flex-col space-y-2 py-2 shadow-lg cursor-pointer'>
@@ -126,7 +128,7 @@ const Investasi = () => {
       <div className='flex flex-col items-center'>
 
         {/* Options Menu */}
-        <div div className='w-full h-fit grid grid-cols-4 lg:gap-12 md:gap-6 gap-4'>
+        <div className='w-full h-fit grid grid-cols-4 lg:gap-12 md:gap-6 gap-4'>
           {
             food.map(element => (
               <div onClick={() => setConfirmFood(element)} className='bg-white flex flex-col space-y-2 py-2 shadow-lg cursor-pointer'>
@@ -147,7 +149,7 @@ const Investasi = () => {
       <div className='flex flex-col items-center'>
 
         {/* Options Menu */}
-        <div div className='w-full h-fit grid grid-cols-4 lg:gap-12 md:gap-6 gap-4'>
+        <div className='w-full h-fit grid grid-cols-4 lg:gap-12 md:gap-6 gap-4'>
           {
             maintenance.map(element => (
               <div onClick={() => setConfirmMaintenance(element)} className='bg-white flex flex-col space-y-2 py-2 shadow-lg cursor-pointer'>
@@ -187,7 +189,7 @@ const Investasi = () => {
         async (resolve) => {
           const phone = resolve.phone.substring(1);
           console.log(phone);
-          const message = `Nama : *${usersName}*\nKambing : *${confirmGoat.name}*\nJenis : *${confirmGoat.type}* (${confirmGoat.weight}/${confirmGoat.time})\nHarga : *${confirmGoat.price}*\nPakan : *${confirmFood.name}*\nPerawatan : *${confirmMaintenance.name}*\n\nKonfirmasi pesanan \n${window.origin}/confirmation/${uid}`;
+          const message = `Nama : *${usersName}*\nKambing : *${confirmGoat.name}*\nJenis : *${confirmGoat.type}* (${confirmGoat.weight}/${confirmGoat.time})\nHarga : *Rp ${confirmGoat.price.toLocaleString().replaceAll(',', '.')}*\nPakan : *${confirmFood.name}*\nPerawatan : *${confirmMaintenance.name}*\n\nKonfirmasi pesanan \n${window.origin}/confirmation/${uid}`;
           const url = `https://api.whatsapp.com/send?phone=62${phone}&text=${encodeURI(message)}`;
           window.open(url);
           await fetch(url).then((res) => {
@@ -251,7 +253,11 @@ const Investasi = () => {
               confirmGoat
                 ? (
                   <div className='flex flex-col space-y-2 py-2 shadow-lg'>
-                    <button onClick={() => setConfirmGoat(null)} className={confirmFood ? 'hidden' : 'mx-2 p-1 self-end bg-gray-400'}><AiOutlineClose size={20} color='#000000' /></button>
+                    <button
+                      onClick={() => setDeleteConfirm({ type: 'goat', show: true })}
+                      className={confirmFood ? 'hidden' : 'mx-2 p-1 self-end bg-gray-400'}>
+                      <AiOutlineClose size={20} color='#000000' />
+                    </button>
                     <img src={confirmGoat.image} alt='/' className='w-full' />
                     <h4 className='font-bold text-[#000000CC] text-base mx-2 overflow-hidden'>{confirmGoat.name} {confirmGoat.sex}</h4>
                     <p className='font-normal text-[#858585] text-sm mx-2 overflow-hidden'>{confirmGoat.type}</p>
@@ -319,6 +325,45 @@ const Investasi = () => {
 
       {/* Footer */}
       <Footer />
+
+      <Modal
+        isOpen={deleteConfirm.show}
+        onRequestClose={() => setDeleteConfirm({ type: '', show: false })}
+        style={{
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            transform: 'translate(-50%, -50%)',
+          },
+          overlay: {
+            backgroundColor: '#000000CC',
+            zIndex: '100',
+          },
+        }}>
+        <div className='bg-white rounded-xl p-4 w-[360px] text-black flex flex-col space-y-6'>
+          <h1 className='text-lg font-bold text-[#333333]'>Yakin ingin menghapus pilihan ini?</h1>
+          <div className='flex justify-end space-x-4'>
+            <button
+              className='px-4 py-2 text-gray-600 border border-gray-400 rounded-lg'
+              onClick={() => setDeleteConfirm({ type: '', show: false })}>
+              Batal
+            </button>
+            <button
+              className='px-4 py-2 bg-red-600 text-white rounded-lg'
+              onClick={() => {
+                if (deleteConfirm.type === 'goat') setConfirmGoat(null);
+                if (deleteConfirm.type === 'food') setConfirmFood(null);
+                if (deleteConfirm.type === 'maintenance') setConfirmMaintenance(null);
+                setDeleteConfirm({ type: '', show: false });
+              }}>
+              Hapus
+            </button>
+          </div>
+        </div>
+      </Modal>
+
 
       {/* Dialog Modal */}
       <Modal
